d7f60d8e1814886ccad56e1fbeed58b57d3da615
Esai Flores <flores.esaijonathan@gmail.com>
2020-08-19 12:25:10 -0500

    getting current day,date, and month

---
+++ main.js
@@ -0,0 +1,13 @@
+function calculateChristmasCountdown() {
+
+    //Get today's date.
+    var now = new Date();
+
+    //Get the current month. Add a +1 because
+    //getMonth starts at 0 for January.
+    var currentMonth = (now.getMonth() + 1);
+
+    //Get the current day of the month.
+    var currentDay = now.getDate();
+
+}
