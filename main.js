8b763c190ee5fdf9c775410e2571ff4f33b5881c
Esai Flores <flores.esaijonathan@gmail.com>
2020-08-19 12:25:52 -0500

    adding functionality that will allow timer to reset

--- main.js
+++ main.js
@@ -10,4 +10,16 @@ function calculateChristmasCountdown() {
     //Get the current day of the month.
     var currentDay = now.getDate();
 
+    //Work out the year that the next Christmas
+    //day will occur on.
+    var nextChristmasYear = now.getFullYear();
+    if(currentMonth == 12 && currentDay > 25){
+        //This year's Christmas Day has already passed.
+        nextChristmasYear = nextChristmasYear + 1;
+    }
+
+    var nextChristmasDate = nextChristmasYear + '-12-25T00:00:00.000Z';
+    var christmasDay = new Date(nextChristmasDate);
+
+
 }
